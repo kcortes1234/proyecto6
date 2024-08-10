a5daaf9462612f7b634ea5bd0dc47dba993755fe
kcortes1234 <134792067+kcortes1234@users.noreply.github.com>
2024-08-09 21:26:07 -0500

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Coursera_T4"
+include(":app")
